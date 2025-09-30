32309a2ba56796dfa5beb10b91bc18f725111f44
momichael98 <momichael98@gmail.com>
2025-09-30 14:20:51 +0900

    DLR basis version

--- class/IPYTH_test/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm ctqmc.*
-rm evalsim.*
-rm *.json
-python3 -u GW_graphene.py $> test.log
--- class/test/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-python3 -u TestCommon.py >> result.log1
-python3 -u TestDyson.py >> result.log2
-python3 -u TestEmbedding.py >> result.log3
-python3 -u TestProjection.py >> result.log4
-python3 -u TestFourierFT.py >> result.log5
-python3 -u TestFourierKR.py >> result.log6
