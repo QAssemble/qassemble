a9cabad4428e6c0c18c62179f049848272c84f88
momichael98 <momichael98@gmail.com>
2026-02-23 11:54:51 +0900

    Changing the Crystal DLR class using modules to double inheritance

--- src/QAssemble/FLatDyn.py
+++ src/QAssemble/FLatDyn.py
@@ -79,7 +79,7 @@ class FLatDyn(Crystal, DLR):
         ns = ftau.shape[2]
         nk = ftau.shape[3]
         ntau = ftau.shape[4]
-        nfreq = len(self.dlr.omega)
+        nfreq = len(self.omega)
         ff = np.zeros((norb,norb,ns,nk,nfreq),dtype=np.complex128,order='F')
         tempmat = np.zeros((ntau), dtype=np.complex128, order='F')
 
@@ -88,7 +88,7 @@ class FLatDyn(Crystal, DLR):
                 
                 for jorb, iorb in itertools.product(range(norb), repeat=2):
                     tempmat = ftau[iorb, jorb, js, ik]
-                    tempmat2 = self.dlr.FT2F(tempmat)
+                    tempmat2 = self.FT2F(tempmat)
                     ff[iorb, jorb, js, ik] = tempmat2
 
         return ff
@@ -108,7 +108,7 @@ class FLatDyn(Crystal, DLR):
             for js in range(ns):
                 for jorb, iorb in itertools.product(range(norb), repeat=2):
                     tempmat = ff[iorb, jorb, js, ik]
-                    tempmat2 = self.dlr.FF2T(tempmat)
+                    tempmat2 = self.FF2T(tempmat)
                     ftau[iorb, jorb, js, ik] = tempmat2
 
         return ftau
@@ -137,9 +137,9 @@ class FLatDyn(Crystal, DLR):
     
     def K2R(self,matk : np.ndarray, rkgrid : list = None) -> np.ndarray:
 
-        rkvec = self.crystal.kpoint
+        rkvec = self.kpoint
         if rkgrid == None:
-            rkgrid = self.crystal.rkgrid
+            rkgrid = self.rkgrid
 
         phases = self._get_fermion_phase()
         norb = matk.shape[0]
@@ -159,7 +159,7 @@ class FLatDyn(Crystal, DLR):
     
     def R2K(self, matr : np.ndarray) -> np.ndarray:
 
-        rkgrid = self.crystal.rkgrid
+        rkgrid = self.rkgrid
 
         norb = matr.shape[0]
         ns = matr.shape[2]
@@ -179,7 +179,7 @@ class FLatDyn(Crystal, DLR):
     
     def R2mR(self) -> list: # move to crystal
 
-        rkvec = self.crystal.kpoint
+        rkvec = self.kpoint
 
         mrkvec = np.array(1.0-rkvec,dtype=float)
 
