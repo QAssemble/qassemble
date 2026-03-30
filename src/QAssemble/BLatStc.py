eeaa353354070cd42a1d5f87872040e9b26e0667
momichael98 <momichael98@gmail.com>
2026-03-30 16:40:27 +0900

    Pulay Mixing

--- src/QAssemble/BLatStc.py
+++ src/QAssemble/BLatStc.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from .BLocStc import VLoc
 from .Crystal import Crystal
-from .utility import Fourier
+from .utility.Fourier import Fourier
 from .utility.Dyson import Dyson
 
 
