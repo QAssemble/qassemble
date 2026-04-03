53aa96fc9ea17d2624bb2787387e9d731174d12b
momichael98 <momichael98@gmail.com>
2026-04-03 13:54:07 +0900

    Modify the GWApproximation with storing z-factor and static limit gw self-energyn

--- src/QAssemble/FLatStc.py
+++ src/QAssemble/FLatStc.py
@@ -41,7 +41,7 @@ class FLatStc(object):
 
         for irk in range(nrk):
             for js in range(ns):
-                matinv[:, :, js, irk] = np.linalg.inv(mat[:, :, js, irk])
+                matinv[:, :, js, irk] = Common.MatInv(mat[:, :, js, irk])
 
         return matinv
 
@@ -1354,7 +1354,7 @@ class ZFactor(FLatStc):
 
         sigma0_dag = np.transpose(np.conjugate(sigma0), (1, 0, 2, 3))
 
-        tempmat = np.asfortranarray(iw * (sigma0 - sigma0_dag), dtype=np.complex128)
+        tempmat = np.asfortranarray(-iw * (sigma0 - sigma0_dag), dtype=np.complex128)
         diag_idx = np.arange(norb)
         tempmat[diag_idx, diag_idx, :, :] += 1.0
 
