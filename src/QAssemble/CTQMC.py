5ba648292f12245d2904426f2d98392a5b4cd596
momichael98 <momichael98@gmail.com>
2026-05-15 09:45:40 +0900

    Add canonical HDF5 saves for impurity DMFT loop

--- src/QAssemble/CTQMC.py
+++ src/QAssemble/CTQMC.py
@@ -153,7 +153,7 @@ class CTQMC(object):
                     params["occupation susceptibility bulla"]=True        
                     params["green bulla"]=True       
                     params["density matrix precise"]=False #True 
-                    params["measurement time"]=10 # 10 # 3 #imp['measurement_time']
+                    params["measurement time"]=3 # 10 # 3 #imp['measurement_time']
 
                     if self._use_dyn():
                         params["dyn"] = {}
